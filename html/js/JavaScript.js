d11a5d9a57212770b758e5dd8be36e53bda43dab
leandrobasualdo <140207714+leandrobasualdo@users.noreply.github.com>
2023-09-05 23:56:04 -0300

    Add files via upload

---
+++ html/js/JavaScript.js
@@ -0,0 +1,9 @@
+let mayor = prompt("Para ingresar a la página debes ser mayor de 18 años. Ingrese su edad");
+
+if(mayor >= 18){
+    alert("Has ingresado a la página de Sailor Moon")
+} else{
+    alert("Entras bajo tu propio riesgo")
+}
+
+console.log(mayor);
